9dbf87adc1ed3055152983b223543d9e2de50808
Rian <88293711+Rianito@users.noreply.github.com>
2022-11-15 10:45:04 -0300

    Update config.py

--- api/app/config.py
+++ api/app/config.py
@@ -6,8 +6,8 @@ class Settings(BaseSettings):
     DEBUG_MODE: bool = False
     HOST: str = "0.0.0.0"
     PORT: int = 80
-    DB_URL: str = "mongodb+srv://riangsilva:7Hs8igqwOUmcAdjr@cluster0.vgcybig.mongodb.net/?retryWrites=true&w=majority"
-    DB_NAME: str = "patients_manager"
+    DB_URL: str = ""
+    DB_NAME: str = ""
 
 
 settings = Settings()
